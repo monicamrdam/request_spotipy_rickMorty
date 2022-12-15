e3290f9c895a4a63179757c1f9b42f1a16a82b4c
monicaMR <monicamrdam@gmail.com>
2022-12-16 00:19:10 +0100

    incorporación urls para usar en la api

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config:
     SERVER_NAME ='127.0.0.1:5000'
     DEBUG= True
 
-    URL_SPOTIFY = 'https://api.spotify.com/v1/search'
+    URL_Search = 'https://api.spotify.com/v1/search'
+    URL_Artist= 'https://api.spotify.com/v1/artists'
 
     CLIENT_ID = os.environ.get('CLIENT_ID', "") # O devuelve la variable de entorno o la cadena vacia
     CLIENTE_SECRET = os.environ.get('CLIENTE_SECRET', "")  # O devuelve la variable de entorno o la cadena vacia
