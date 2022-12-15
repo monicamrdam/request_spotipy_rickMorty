8ef874183c4db4edbcdc9213a6eb4cc51192c064
monicaMR <monicamrdam@gmail.com>
2022-12-15 21:34:06 +0100

    archivo config.py

---
+++ app/artist/artist_controller.py
@@ -0,0 +1,4 @@
+from app import app
+from artist.artist_service import SpotifyService
+
+#Creamos una ruta de prueba home para tester que el servitor funciona
---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+#Carga las variables de entorno en memoria
+load_dotenv() #Cargar todo el contenido de .env en variables de entorno
+
+class Config:
+    SERVER_NAME ='127.0.0.1:5000'
+    DEBUG= True
+
+    URL_SPOTIFY = 'https://api.spotify.com/v1/search'
+
+    CLIENT_ID = os.environ.get('CLIENT_ID', "") # O devuelve la variable de entorno o la cadena vacia
+    CLIENTE_SECRET = os.environ.get('CLIENTE_SECRET', "")  # O devuelve la variable de entorno o la cadena vacia
