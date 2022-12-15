c0d0bb52d9a25841e2a5e40239575c1d4e2df67d
monicaMR <monicamrdam@gmail.com>
2022-12-15 21:36:38 +0100

    routes e init incorporado rutas nuevas

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
 from app import app
 from flask import jsonify
+from app.artist.artist_service import SpotifyService
+from app.artist.spotify_client import SpotifyClient
 
-#Creamos una ruta de prueba home para tester que el servitor funciona
 @app.route('/')
 def home():
     message={
         "Home":'http://127.0.0.1:5000/',
+        "Artist": 'http://127.0.0.1:5000/artist',
     }
     return jsonify(message)
+
+
+@app.route('/artist')
+def spotifyService():
+    return SpotifyClient.authorization()
