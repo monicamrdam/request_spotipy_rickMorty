441f1199f7bdf66deceda64d59e56c9b13a7931d
monicaMR <monicamrdam@gmail.com>
2022-12-15 21:37:55 +0100

    class artist y topTrack

---
+++ app/artist/artist.py
@@ -0,0 +1,6 @@
+class Artist:
+    def __init__(self, name: str, popularity: int, uri: str,gender:[] ):
+        self.name=name
+        self.popularity = popularity
+        self.uri=uri
+        self.gender=gender
---
+++ app/artist/artist_topTracks.py
@@ -0,0 +1,5 @@
+class Top_Tracks:
+    def __init__(self, name:str, popularity:int, audioSong: str):
+        self.name = name
+        self.popularity= popularity
+        self.audioSong=audioSong
