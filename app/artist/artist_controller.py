79c359c9b4597fea247d9f5fe3bd419b4c3c57fc
monicaMR <monicamrdam@gmail.com>
2022-12-15 21:38:54 +0100

    borrado datos de controller y service

--- app/artist/artist_controller.py
+++
@@ -1,4 +0,0 @@
-from app import app
-from artist.artist_service import SpotifyService
-
-#Creamos una ruta de prueba home para tester que el servitor funciona
---
+++ app/artist/artist_service.py
@@ -0,0 +1,8 @@
+
+class SpotifyService:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def response():
+        return "Home artist"
