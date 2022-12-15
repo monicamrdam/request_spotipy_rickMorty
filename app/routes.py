ac1ead969407ca6a5259149f8054ea6dc65b147c
monicaMR <monicamrdam@gmail.com>
2022-12-15 22:50:37 +0100

    route que obtiene argumento de la url

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import request
 from app import app
 from flask import jsonify
 from app.artist.artist_service import SpotifyService
@@ -7,11 +8,12 @@ from app.artist.spotify_client import SpotifyClient
 def home():
     message={
         "Home":'http://127.0.0.1:5000/',
-        "Artist": 'http://127.0.0.1:5000/artist',
+        "Artist": 'http://127.0.0.1:5000/artist?name=',
     }
     return jsonify(message)
 
 
 @app.route('/artist')
-def spotifyService():
-    return SpotifyClient.authorization()
+def spotify_client():
+    name= request.args.get('name', type=str)
+    return SpotifyClient.authorization(name)
