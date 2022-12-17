925a478c7cdbeef4b9d0a45d35ec6461412d11ac
monicaMR <monicamrdam@gmail.com>
2022-12-17 08:16:41 +0100

    Creación de la ruta character

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,24 @@ from flask import request
 from app import app
 from flask import jsonify
 from app.artist.artist_service import SpotifyService
-from app.artist.spotify_client import SpotifyClient
+from app.character.character_service import RickAndMortyService
 
 @app.route('/')
 def home():
     message={
         "Home":'http://127.0.0.1:5000/',
         "Artist": 'http://127.0.0.1:5000/artist?name=',
+        "Character": 'http://127.0.0.1:5000/rickandmorty',
     }
     return jsonify(message)
 
 
 @app.route('/artist')
-def spotify_client():
+def spotify_service():
     name= request.args.get('name', type=str)
     return SpotifyService.get_artist_popularity(name)
+
+
+@app.route('/rickandmorty')
+def rickandmorty_service():
+    return RickAndMortyService.listCharacter()
